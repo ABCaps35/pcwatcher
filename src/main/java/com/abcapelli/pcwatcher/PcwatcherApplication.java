bf8d000aa93d531d411b93e80772472a9c974b45
ABCaps35 <acapelli345@gmail.com>
2022-04-22 11:25:11 -0400

    initial commit

---
+++ src/main/java/com/abcapelli/pcwatcher/PcwatcherApplication.java
@@ -0,0 +1,13 @@
+package com.abcapelli.pcwatcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PcwatcherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PcwatcherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/abcapelli/pcwatcher/PcwatcherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abcapelli.pcwatcher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PcwatcherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
