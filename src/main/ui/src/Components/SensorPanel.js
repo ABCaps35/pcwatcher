ffde38d12f56a8b5505b1884887474e71fe6da98
ABCaps35 <acapelli345@gmail.com>
2022-04-25 17:04:22 -0400

    adds initial UI for sensor panel

---
+++ src/main/ui/src/Components/SensorPanel.js
@@ -0,0 +1,226 @@
+import { Grid, Box, Paper, Card } from "@mui/material";
+import { experimentalStyled as styled } from '@mui/material/styles';
+import { Bar, Doughnut } from 'react-chartjs-2';
+import {
+    Chart as ChartJS,
+    ArcElement,
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    Title,
+    Tooltip,
+    Legend,
+  } from 'chart.js';
+import React from "react";
+
+const Item = styled(Paper)(({ theme }) => ({
+    backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+    ...theme.typography.body2,
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  }));
+
+const SensorPanel = (props) => {
+    const { data, orientation } = props;
+    
+    const maxes = {
+        "dram_load": 100,
+        "vram_load": 100,
+        "cpu_load": 100,
+        "gpu_load": 100,
+        "cpu_temp": 95, 
+        "gpu_temp": 90
+    }
+
+    ChartJS.register(
+        ArcElement,
+        CategoryScale,
+        LinearScale,
+        BarElement,
+        Title,
+        Tooltip,
+        Legend
+    );
+
+    const donut_options = {
+        responsive: true,
+        plugins: {
+            tooltip: {enabled: false},
+            legend: {display: false},
+            title: {display: false},
+        },
+        scales: {
+            xAxis: {display: false},
+            yAxis: {display: false}
+        }
+    };
+
+    const bar_options = {
+        indexAxis: 'y',
+        elements: {
+            bar: {borderWidth: 2}
+        },
+        responsive: true,
+        plugins: {
+            tooltip: {enabled: false},
+            legend: {display: false},
+            title: {display: false},
+        },
+        scales: {
+            xAxis: {min: 0, max: 100},
+            yAxis: {display: false}
+        }
+    };
+
+    const cpu_load_data = {
+        labels: ['CPU Load'],
+        datasets: [
+            {
+                label: 'CPU Load',
+                data: [data.cpu_load,100-data.cpu_load],
+                borderColor: ['rgb(0,255,0)','rgb(0,0,0)'],
+                backgroundColor: ['rgb(150,255,150)','rgb(0,0,0)'],
+                circumference: 300,
+                rotation: 210
+            }
+        ]
+    }
+    const gpu_load_data = {
+        labels: ['GPU Load'],
+        datasets: [
+            {
+                label: 'GPU Load',
+                data: [data.gpu_load,100-data.gpu_load],
+                borderColor: ['rgb(255,0,0)','rgb(0,0,0)'],
+                backgroundColor: ['rgb(255,150,150','rgb(0,0,0)'],
+                circumference: 300,
+                rotation: 210
+            }
+        ]
+    }
+
+    const cpu_temp_data = {
+        labels: ['CPU Temp'],
+        datasets: [
+            {
+                label: 'CPU Temp',
+                data: [data.cpu_temp,95-data.cpu_temp],
+                borderColor: ['rgb(0,255,0)','rgb(0,0,0)'],
+                backgroundColor: ['rgb(150,255,150)','rgb(0,0,0)'],
+                circumference: 300,
+                rotation: 210
+            }
+        ]
+    }
+    const gpu_temp_data = {
+        labels: ['GPU Temp'],
+        datasets: [
+            {
+                label: 'GPU Temp',
+                data: [data.gpu_temp,90-data.gpu_temp],
+                borderColor: ['rgb(255,0,0)','rgb(0,0,0)'],
+                backgroundColor: ['rgb(255,150,150','rgb(0,0,0)'],
+                circumference: 300,
+                rotation: 210
+            }
+        ]
+    }
+
+    const dram_data = {
+        labels: ['DRAM'],
+        datasets: [
+            {
+                label: 'DRAM',
+                data: [data.dram_load],
+                borderColor: 'rgb(0,255,0)',
+                backgroundColor: 'rgb(150,255,150)'
+            }
+        ]
+    };
+    const vram_data = {
+        labels: ['VRAM'],
+        datasets: [
+            {
+                label: 'VRAM',
+                data: [data.vram_load],
+                borderColor: 'rgb(255,0,0)',
+                backgroundColor: 'rgb(255,150,150)'
+            },
+        ],
+    };
+
+    return (
+        <Box>
+            {orientation==='horizontal' ? ( 
+                <Grid container alignItems='stretch' spacing={3} p={3}>
+                    <Grid item xs={6} md={4} style={{display: 'flex'}}>
+                        <Card sx={{p: 2}}>CPU Load: {data.cpu_load}%
+                            <Doughnut options={donut_options} data={cpu_load_data}/>
+                        </Card>
+                    </Grid>
+                    <Grid item xs={6} md={4} style={{display: 'flex'}}>
+                        <Card sx={{p: 2}}>CPU Temp: {data.cpu_temp} °C
+                            <Doughnut options={donut_options} data={cpu_temp_data}/>
+                        </Card>
+                    </Grid>
+                    <Grid item xs={6} md={4} style={{display: 'flex'}}>
+                        <Card sx={{p: 2}}>DRAM Load: {data.dram_load}%
+                            <Bar options={bar_options} data={dram_data}/>
+                        </Card>
+                    </Grid>
+                    <Grid item xs={6} md={4} style={{display: 'flex'}}>
+                        <Card sx={{p: 2}}>GPU Load: {data.gpu_load}%
+                            <Doughnut options={donut_options} data={gpu_load_data}/>
+                        </Card>
+                    </Grid>
+                    <Grid item xs={6} md={4} style={{display: 'flex'}}>
+                        <Card sx={{p: 2}}>GPU Temp: {data.gpu_temp} °C
+                            <Doughnut options={donut_options} data={gpu_temp_data}/>
+                        </Card>
+                    </Grid>
+                    <Grid item xs={6} md={4} style={{display: 'flex'}}>
+                        <Card sx={{p: 2}}>VRAM Load: {data.vram_load}%
+                            <Bar options={bar_options} data={vram_data}/>
+                        </Card>
+                    </Grid>
+                </Grid>
+            ): (
+                <Grid container spacing={3} p={3}>
+                    <Grid item xs={6} md={4} style={{display: 'flex'}}>
+                        <Card sx={{p: 2}}>CPU Load: {data.cpu_load}%
+                            <Doughnut options={donut_options} data={cpu_load_data}/>
+                        </Card>
+                    </Grid>
+                    <Grid item xs={6} md={4} style={{display: 'flex'}}>
+                        <Card sx={{p: 2}}>GPU Load: {data.gpu_load}%
+                            <Doughnut options={donut_options} data={gpu_load_data}/>
+                        </Card>
+                    </Grid>
+                    <Grid item xs={6} md={4}>
+                        <Card sx={{p: 2}}>CPU Temp: {data.cpu_temp} °C
+                            <Doughnut options={donut_options} data={cpu_temp_data}/>
+                        </Card>
+                    </Grid>
+                    <Grid item xs={6} md={4}>
+                        <Card sx={{p: 2}}>GPU Temp: {data.gpu_temp} °C
+                            <Doughnut options={donut_options} data={gpu_temp_data}/>
+                        </Card>
+                    </Grid>
+                    <Grid item xs={6} md={4}>
+                        <Card sx={{p: 2}}>DRAM Load: {data.dram_load}%
+                            <Bar options={bar_options} data={dram_data}/>
+                        </Card>
+                    </Grid>
+                    <Grid item xs={6} md={4}>
+                        <Card sx={{p: 2}}>VRAM Load: {data.vram_load}%
+                            <Bar options={bar_options} data={vram_data}/>
+                        </Card>
+                    </Grid>
+                </Grid>
+            )}
+        </Box>
+    )
+}
+
+export default SensorPanel;
--- src/main/ui/src/Views/Main.js
+++ src/main/ui/src/Views/Main.js
@@ -1,38 +1,45 @@
 import React, {useState, useEffect} from 'react';
 import axios from 'axios';
-import {Box} from '@mui/material';
+import {Box, Button} from '@mui/material';
+import SensorPanel from '../Components/SensorPanel';
 
 const Main = (props) => { 
     const [data, setData] = useState([]);
     const [loaded, setLoaded] = useState(false);
+    const [reload, setReload] = useState(false);
 
     useEffect(() => {
         console.log('effect');
         axios.get('http://localhost:8080/api/fetch/')
         .then(response=>{
-            setData(response);
+            setData(response.data);
             setLoaded(true);
-            console.log(response);
+            setReload(false);
+            console.log(response.data);
         })
         .catch(err=>console.error(err));
-    },[])
+    },[reload])
 
-    /*
-    const removeFromDom = (pirateId) => {
-        setPirates(pirates.filter(pirate => pirate._id !== pirateId));
+    const refresh = () => {
+        //setLoaded(false);
+        setReload(true);
     }
-    */
 
     return(
         <div>
             <Box sx={{display: {xs: 'block', sm: 'none', md: 'none'}}}>
-                <p>XS</p>
+                {loaded && <SensorPanel data={data} orientation="vertical" />}
             </Box>
             <Box sx={{display: {xs: 'none', sm: 'block', md: 'none'}}}>
-                <p>S</p>
+                {loaded && <SensorPanel data={data} orientation="vertical" />}
             </Box>
             <Box sx={{display: {xs: 'none', sm: 'none', md: 'block'}}}>
-                <p>MD</p>
+                {loaded && <SensorPanel data={data} orientation="horizontal" />}
+            </Box>
+            <Box sx={{mx: 'auto'}}>
+                <Button variant="contained" onClick={refresh}>
+                    Fetch Data
+                </Button>
             </Box>
         </div>
     )
